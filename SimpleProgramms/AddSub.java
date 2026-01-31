90f844d8c42dede91fab2e3466a43a953ea44612
Taufeeq6t5 <taufeeqahmed.iet@srinivasuniversity.edu.in>
2026-01-31 21:50:37 +0530

    Updated AddSub Program

--- SimpleProgramms/AddSub.java
+++ SimpleProgramms/AddSub.java
@@ -6,7 +6,11 @@ public class AddSub {
         int i = 10;
         int j = 15;
         int sum = 0;
+        int sum1=0;
         sum = i + j;
         System.out.println("Sum of two Number is : " + sum);
+
+        sum1 = i - j;
+        System.out.println("Sub of two number is : " + sum1 );
     }
 }
