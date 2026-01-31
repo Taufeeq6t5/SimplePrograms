e4a0b02e00ac4f0056b6f390087d5841de46fa67
Taufeeq6t5 <taufeeqahmed.iet@srinivasuniversity.edu.in>
2026-01-31 21:37:56 +0530

    Added Fisrt Program

---
+++ SimpleProgramms/AddSub.java
@@ -0,0 +1,12 @@
+
+public class AddSub {
+
+    public static void main(String[] args) {
+
+        int i = 10;
+        int j = 15;
+        int sum = 0;
+        sum = i + j;
+        System.out.println("Sum of two Number is : " + sum);
+    }
+}
