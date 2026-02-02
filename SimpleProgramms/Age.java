56d63b79202508f08120fd2d10a59c16823b4aeb
Taufeeq6t5 <taufeeqahmed.iet@srinivasuniversity.edu.in>
2026-02-02 14:30:20 +0530

    Updated program to calculate age using DOB

--- SimpleProgramms/Age.java
+++ SimpleProgramms/Age.java
@@ -1,14 +1,26 @@
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Scanner;
+
 public class Age {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
-        System.out.println("Enter your age:");
-        int a=sc.nextInt();
-        if(a>=18){
-            System.err.println("Your age is :"+ a +"\n you are eligible to vote..");
+        System.out.println("Enter your date of birth (YYYY-MM-DD) :");
+        String dobInput=sc.next();
+
+        LocalDate dob=LocalDate.parse(dobInput);
+        LocalDate today=LocalDate.now();
+
+        int age=Period.between(dob,today).getYears();
+
+        System.out.println("Your age is : "+ age);
+
+
+         if(age>=18){
+            System.err.println( "You are eligible to vote..");
         }else{
-            System.err.println("Your age is :"+ a +"\n you are not eligible to vote..!");
+            System.err.println("You are not eligible to vote..!");
         }
         sc.close();
     }
