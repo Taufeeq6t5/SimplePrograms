d093fdde5fdc9d47024eb03f5c489aed23208374
Taufeeq Ahmed <taufeeq6t5@gmail.com>
2026-02-04 18:16:15 +0530

    Update AddSub.java

--- SimpleProgramms/AddSub.java
+++ SimpleProgramms/AddSub.java
@@ -5,12 +5,24 @@ public class AddSub {
 
         int i = 10;
         int j = 15;
+        
         int sum = 0;
-        int sum1=0;
         sum = i + j;
         System.out.println("Sum of two Number is : " + sum);
 
+        int sub=0;
         sum1 = i - j;
-        System.out.println("Sub of two number is : " + sum1 );
+        System.out.println("Sub of two number is : " + sub );
+
+        //Trying with decimal values 
+        float a = 35.62;
+        float b = 12.34;
+
+        float sum1 = 0;
+        System.out.println("Sum of two Number is : " + sum1);
+
+        float sub1 = 0;
+        System.out.println("Sub of two number is : " + sub1 );
+        
     }
 }
