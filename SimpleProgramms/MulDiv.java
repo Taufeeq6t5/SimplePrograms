c25d75e99adcc697cee1e47194e90fd88619ce46
Taufeeq Ahmed <taufeeq6t5@gmail.com>
2026-02-04 18:22:52 +0530

    Update MulDiv.java

--- SimpleProgramms/MulDiv.java
+++ SimpleProgramms/MulDiv.java
@@ -1,14 +1,15 @@
 public class MulDiv {
     public static void main(String[] args) {
-        int i = 6;
+        int i = 12;
         int j = 8;
         int sum = 0;
-        int sum1=0;
         sum = i * j;
         System.out.println("Multiple of two Number is : " + sum);
 
-        int k=50;
-        int p=10;
+        
+        double sum1 = 0;
+        double k=56;
+        double p=10;
         sum1 = k / p;
         System.out.println("Division of two number is : " + sum1 );
     }
